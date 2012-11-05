26697f64512ce7a481cf8490706988e3df263887
Alexander Ulitin <g91@yandex.ru>
2012-11-05 19:02:06 +0600

    capistrano-unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :domain, "interactiff@interactiff.net"
 set :port, 60321
 set :user, "interactiff"
 ssh_options[:forward_agent] = true #заставляет сервер юзать ключи компьютера
-set :repository, "git@github.com:interactiff/joygen.git"
+set :repository, "git@github.com:ulitiy/joygen.git"
 set :scm, "git"
 set :branch, "master"
 set :use_sudo, false
