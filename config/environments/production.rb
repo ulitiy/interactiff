a5f68d72693f573e31366d1f5719bc9bf58abe9c
Alexander Ulitin <g91@yandex.ru>
2013-02-13 20:07:36 +0400

    production env assets digest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,7 @@ Interactiff::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.digest = true
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
