a93eb1dbf4bebedaedde60a75e2451909fe00303
Alexander Ulitin <g91@yandex.ru>
2013-03-25 13:22:23 +0400

    compact event array bug fix

--- app/models/event_handler.rb
+++ app/models/event_handler.rb
@@ -46,7 +46,7 @@ class EventHandler
   def input input
     if current_task?
       task_answers.to_a.find { |answer| @events=answer.hit(options.merge(input: input)) }
-      return @events
+      return @events.compact
     end
   end
 
