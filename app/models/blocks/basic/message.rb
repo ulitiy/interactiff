9e3f9274315443e7e7f67953e485c3e0d1a1e6e3
Alexander Ulitin <g91@yandex.ru>
2013-03-29 00:27:07 +0400

    change default message type to notice

--- app/models/blocks/basic/message.rb
+++ app/models/blocks/basic/message.rb
@@ -1,5 +1,5 @@
 class Message < Block
   field :message, type: String, :default => ""
-  field :message_type, type: String, :default => "info"
+  field :message_type, type: String, :default => "notice"
   attr_accessible :message, :message_type
 end
