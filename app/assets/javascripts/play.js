8018a46daa3783a0dfd069eb2a2e437d67f328cd
Alexander Ulitin <g91@yandex.ru>
2013-04-06 23:07:13 +0400

    task passed in play ui, refresh on browser back

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,3 +1,9 @@
 //= require jquery
 //= require twitter/bootstrap
 //= require countdown
+
+onload=function(){
+var e=document.getElementById("refreshed");
+if(e.value=="no")e.value="yes";
+else{e.value="no";location.reload();}
+}
