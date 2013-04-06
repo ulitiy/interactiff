3ed7c5775a19375d0b754ac39b93a3005babdc0e
Alexander Ulitin <g91@yandex.ru>
2013-04-07 00:39:44 +0400

    focus in input in play UI

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,9 +1,4 @@
 //= require jquery
 //= require twitter/bootstrap
 //= require countdown
-
-onload=function(){
-var e=document.getElementById("refreshed");
-if(e.value=="no")e.value="yes";
-else{e.value="no";location.reload();}
-}
+//= require_tree ./play
