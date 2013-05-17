1159a8efebb2344e67b8e49947ec1d112bfdd12a
Alexander Ulitin <g91@yandex.ru>
2013-05-18 01:21:13 +0400

    .

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -1,5 +1,42 @@
 # encoding: utf-8
 
+module RoutingFilter
+  class RefineryLocales < Filter
+
+    def around_recognize(path, env, &block)
+      if ::Refinery::I18n.url_filter_enabled?
+        if path =~ %r{^/(#{::Refinery::I18n.locales.keys.join('|')})(/|$)}
+          path.sub! %r(^/(([a-zA-Z\-_])*)(?=/|$)) do
+            ::I18n.locale = $1
+            ''
+          end
+          path.sub!(%r{^$}) { '/' }
+        # else
+          # ::I18n.locale = ::Refinery::I18n.default_frontend_locale
+        end
+      end
+
+      yield.tap do |params|
+        params[:locale] = ::I18n.locale if ::Refinery::I18n.enabled?
+      end
+    end
+
+    def around_generate(params, &block)
+      locale = params.delete(:locale) || ::I18n.locale
+
+      yield.tap do |result|
+        result = result.is_a?(Array) ? result.first : result
+        if ::Refinery::I18n.url_filter_enabled? and
+           # locale != ::Refinery::I18n.default_frontend_locale and
+           result !~ %r{^/(#{Refinery::Core.backend_route}|wymiframe)}
+          result.sub!(%r(^(http.?://[^/]*)?(.*))) { "#{$1}/#{locale}#{$2}" }
+        end
+      end
+    end
+
+  end
+end
+
 Refinery::I18n.configure do |config|
   config.enabled = true
 
@@ -7,7 +44,7 @@ Refinery::I18n.configure do |config|
 
   config.current_locale = :ru
 
-  config.default_frontend_locale = :fr
+  config.default_frontend_locale = :ru
 
   config.frontend_locales = [:ru, :en]
 
