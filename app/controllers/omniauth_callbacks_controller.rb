73131cad9f6568f8d53c6623bb0998e6cdab8485
Alexander <cio@mugbe.com>
2013-06-07 01:25:40 +0600

    .

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -12,32 +12,26 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       current_user.accounts.create!(omniauth.slice(:provider, :uid))
       redirect_to games_url, notice: "#{omniauth[:provider]} successfuly added to your account"
     elsif user = create_or_get_user(omniauth)
-      # Register the new user + create new account
+      user.accounts.create!(omniauth.slice(:provider, :uid))
       flash[:notice] = 'Welcome to interactiff'
       user.remember_me = true
       sign_in_and_redirect user
+    else
+      # New user data not valid, try again
+      flash[:alert] = "Fail auth"
+      redirect_to new_user_registration_url
     end  
   end
   def create_or_get_user(omniauth)
-    mail = (omniauth[:user_info].present? && omniauth[:user_info][:email].present?) ? omniauth[:user_info][:email] : "#{omniauth[:provider]}_#{omniauth[:uid]}@interactiff.net"
-    user = User.where(email: mail).first || User.new
-    if user.new_record?
-      user.email = mail
-      account = user.accounts.where(omniauth.slice(:provider, :uid)).first || user.accounts.new
-      if account.new_record?
-        account.uid = omniauth[:uid]
-        account.provider = omniauth[:provider]
-      end
-      user.save(validate: false)
-    else
-      account = user.accounts.where(omniauth.slice(:provider, :uid)).first || user.accounts.new
-      if account.new_record?
-        account.uid = omniauth[:uid]
-        account.provider = omniauth[:provider]
-        account.save!
-      end
+    user_info = omniauth['user_info']
+    if omniauth['extra'] && omniauth['extra']['user_hash']
+      user_info.merge!(omniauth['extra']['user_hash'])
     end
-    user
+    user = User.where(email: user_info['email']).first || User.new
+    if user.new_record?
+      user.email = user_info['email']
+      user.save(validate: false)
+    end
   end
   alias_method :facebook, :all
   alias_method :vkontakte, :all
